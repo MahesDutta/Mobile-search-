fd7754ea6f62a7fc8379e8b54c80f13f4c30a9b1
MahesDutta <112416184+MahesDutta@users.noreply.github.com>
2026-05-05 12:29:05 +0530

    Fix formatting of Turnstile component siteKey

--- src/components/RemoveDataModal.tsx
+++ src/components/RemoveDataModal.tsx
@@ -178,8 +178,8 @@ export default function RemoveDataModal({ isOpen, onClose }: RemoveDataModalProp
 
                                 <div className="flex justify-center mb-4">
                                     <Turnstile
-                                        siteKey="0x4AAAAAADJSWYkT3lgBCCm_
-                                        "
+                                        siteKey="0x4AAAAAADJSWYkT3lgBCCm_"
+             
                                         options={{ theme: 'light' }}
                                         onSuccess={(token) => {
                                             setToken(token);
